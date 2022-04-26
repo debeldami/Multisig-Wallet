894be40a2ef1fcdeb9d28e4e75389a630b4db76f
debeldami <debeldamilola@gmail.com>
2022-04-26 12:58:36 +0100

    work on create transfer, get transfer

--- wallet.sol
+++ wallet.sol
@@ -2,11 +2,36 @@
 pragma solidity 0.8.13;
 
 contract wallet{
+
     address[] public approvers;
+
     uint public quorum;
+
+    struct Transfer{
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+
+    Transfer[] public transfers;
 
     constructor(address[] memory _approvers, uint _quorum){
       approvers = _approvers;
       quorum = _quorum;
+    }
+
+
+    function getApprovers() external view returns (address[] memory){
+        return approvers;
+    }
+
+    function getTransfer() external view returns (Transfer[] memory){
+        return transfers;
+    }
+
+    function createTransfer(uint amount, address payable to) external{
+        transfers.push(Transfer(transfers.length, amount, to, 0, false));
     }
 }
