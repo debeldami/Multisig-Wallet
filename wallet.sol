10d5085f661df5b4004c6a323a16a8ee0e6f82ee
debeldami <debeldamilola@gmail.com>
2022-04-26 10:34:39 +0100

    constructor declaration

---
+++ wallet.sol
@@ -0,0 +1,12 @@
+//SPDX-lincense-identifier: MIT
+pragma solidity 0.8.13;
+
+contract wallet{
+    address[] public approvers;
+    uint public quorum;
+
+    constructor(address[] memory _approvers, uint _quorum){
+      approvers = _approvers;
+      quorum = _quorum;
+    }
+}
