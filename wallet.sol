0851a7e2430cee527b345d6cb936b7d785b310d0
debeldami <debeldamilola@gmail.com>
2022-04-26 15:27:14 +0100

    implement approve transfer function

--- wallet.sol
+++ wallet.sol
@@ -16,6 +16,7 @@ contract wallet{
     }
 
     Transfer[] public transfers;
+    mapping(address => mapping(uint => bool)) approvals;
 
     constructor(address[] memory _approvers, uint _quorum){
       approvers = _approvers;
@@ -33,5 +34,20 @@ contract wallet{
 
     function createTransfer(uint amount, address payable to) external{
         transfers.push(Transfer(transfers.length, amount, to, 0, false));
+    }
+
+    function approveTransfer(uint id) external{
+        require(transfers[id].sent == false, "transfer already completed");
+        require(approvals[msg.sender][id] == false, "cannot approve transfer twice");
+
+        approvals[msg.sender][id] = true;
+        transfers[id].approvals++;
+
+        if(transfers[id].approvals >= quorum){
+            transfers[id].sent = true;
+            address payable to = transfers[id].to;
+            uint amount = transfers[id].amount;
+            to.transfer(amount);
+        }
     }
 }
